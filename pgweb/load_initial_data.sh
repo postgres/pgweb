c24eea14ab25b1d6ccd42a617a4763364c894738
Magnus Hagander <magnus@hagander.net>
2009-09-16 13:16:04 +0200

    Initial data must not live in initial_data.yaml, that will cause the data that's currently in the database to be overwritten... Put it in a separate file, and add a script that will automatically load all these files if present.

---
+++ pgweb/load_initial_data.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# We keep this in a separate script because using initial_data.xxx in django will overwrite
+# critical data in the database when running a 'syncdb'. We'd like to keep the ability to
+# run syncdb on updates...
+
+echo WARNING: this may overwrite some data in the database with an initial set of data.
+echo 'Are you sure you want this (answer "yes" to overwrite)'
+read R
+
+if [ "$R" == "yes" ]; then
+   find . -name data.yaml | xargs ./manage.py loaddata
+fi
