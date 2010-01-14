3afa5ccbb92ccdddc55b9ca871805a2aca9f496d
Magnus Hagander <magnus@hagander.net>
2010-01-14 22:03:01 +0100

    Nicer admin of quotes

--- pgweb/quotes/admin.py
+++ pgweb/quotes/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from models import *
 
-admin.site.register(Quote)
+class QuoteAdmin(admin.ModelAdmin):
+	list_display = ('quote', 'who', 'org', )
+
+admin.site.register(Quote, QuoteAdmin)
