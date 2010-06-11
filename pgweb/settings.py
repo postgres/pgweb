81caa54e1bdb0b2fe2fec8c0d3164287983dccf8
Magnus Hagander <magnus@hagander.net>
2010-06-11 15:58:03 +0200

    Clean up the admin interface - move our special links to the index page only, remove them from the login form. Remove the Sites application, since we don't use it.

--- pgweb/settings.py
+++ pgweb/settings.py
@@ -88,7 +88,6 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
     'django.contrib.admin',
     'django.contrib.markup',
     'pgweb.core',
