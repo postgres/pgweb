af82e7cb873d909d7ec35743b5100a8bdc1d4616
Magnus Hagander <magnus@hagander.net>
2011-08-16 21:00:59 +0200

    When voting on a survey without a vote, redirect to results
    instead of showing an error. This is now the same behaviour as the old site.

--- pgweb/survey/views.py
+++ pgweb/survey/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseServerError, HttpResponseRedirect
 from django.db import connection
+from django.template.defaultfilters import slugify
 
 from pgweb.util.contexts import NavContext
 from pgweb.util.misc import get_client_ip
@@ -17,13 +18,16 @@ def results(request, surveyid, junk=None):
 	}, NavContext(request, 'community'))
 
 def vote(request, surveyid):
+	surv = get_object_or_404(Survey, pk=surveyid)
+
 	# Check that we have a valid answer number
 	try:
 		ansnum = int(request.POST['answer'])
 		if ansnum < 1 or ansnum > 8:
 			return HttpResponseServerError("Invalid answer")
 	except:
-		return HttpResponseServerError("Unable to determine answer")
+		# When no answer is given, redirect to results instead
+		return HttpResponseRedirect("/community/survey/%s-%s" % (surv.id, slugify(surv.question)))
 	attrname = "tot%s" % ansnum
 
 	# Do IP based locking...
@@ -42,8 +46,6 @@ def vote(request, surveyid):
 	lock = SurveyLock(ipaddr=addr)
 	lock.save()
 
-	# Only now do we bother actually finding out if the survey exists...
-	surv = get_object_or_404(Survey, pk=surveyid)
 	answers = SurveyAnswer.objects.get_or_create(survey=surv)[0]
 	setattr(answers, attrname, getattr(answers, attrname)+1)
 	answers.save()
