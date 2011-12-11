a331c1f677c4ee47b0b46f720cf7353cec23f194
Magnus Hagander <magnus@hagander.net>
2011-12-11 14:15:41 +0100

    Add a varnish_purge() function to be used in local installs

---
+++ sql/varnish_local.sql
@@ -0,0 +1,15 @@
+BEGIN;
+
+--
+-- "cheating" version of the varnish_purge() function
+-- that can be used on a local installation that doesn't
+-- have any frontends.
+--
+
+CREATE OR REPLACE FUNCTION varnish_purge(url text)
+RETURNS bigint
+AS $$
+   SELECT 1::bigint;
+$$ LANGUAGE 'sql';
+
+COMMIT;
