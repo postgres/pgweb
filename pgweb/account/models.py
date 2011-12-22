061e33e9a80cd1b908fcf90987015c02afeb611b
Magnus Hagander <magnus@hagander.net>
2011-12-22 19:19:54 +0100

    Add hint about how to create a key

--- pgweb/account/models.py
+++ pgweb/account/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class CommunityAuthSite(models.Model):
 	name = models.CharField(max_length=100, null=False, blank=False)
 	redirecturl = models.URLField(max_length=200, null=False, blank=False)
-	cryptkey = models.CharField(max_length=100, null=False, blank=False)
+	cryptkey = models.CharField(max_length=100, null=False, blank=False,
+								help_text="Use tools/communityauth/generate_cryptkey.py to create a key")
 	comment = models.TextField(null=False, blank=True)
 
 	def __unicode__(self):
