1fe8d6055b94cff3e76d631c243406c87caddbc1
Magnus Hagander <magnus@hagander.net>
2011-12-22 19:18:17 +0100

    Add comment field to community sites, never hurts...

--- pgweb/account/models.py
+++ pgweb/account/models.py
@@ -4,6 +4,7 @@ class CommunityAuthSite(models.Model):
 	name = models.CharField(max_length=100, null=False, blank=False)
 	redirecturl = models.URLField(max_length=200, null=False, blank=False)
 	cryptkey = models.CharField(max_length=100, null=False, blank=False)
+	comment = models.TextField(null=False, blank=True)
 
 	def __unicode__(self):
 		return self.name
