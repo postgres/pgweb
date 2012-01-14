c461f9f0c669fe177f6e55c47adb81de86c6b095
Magnus Hagander <magnus@hagander.net>
2012-01-14 17:20:45 +0100

    Include support/versioning in search results again!

--- pgweb/core/struct.py
+++ pgweb/core/struct.py
@@ -4,6 +4,7 @@ from datetime import datetime
 def get_struct():
 	yield ('', None)
 	yield ('community/', None)
+	yield ('support/versioning', None)
 
 	# Enumerate all the templates that will generate pages
 	for root, dirs, files in os.walk('../templates/pages'):
