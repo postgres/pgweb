cf634347bfecad1e7ddcb4ac753cb0d64ecded01
Magnus Hagander <magnus@hagander.net>
2012-02-13 10:27:11 +0100

    Temporary remove listing of PWN from the website
    Given that new PWNs aren't actually posted into the database, let's not
    have the website pretend that they are there.
    
    Once we're ready to post PWNs to the website again, this commit should
    be reverted.

--- pgweb/pwn/struct.py
+++ pgweb/pwn/struct.py
@@ -6,9 +6,3 @@ def get_struct():
 	now = date.today()
 
 	yield ('community/weeklynews/', None)
-	for p in PwnPost.objects.all():
-		yearsold = (now - p.date).days / 365
-		if yearsold > 4:
-			yearsold = 4
-		yield ('community/weeklynews/pwn%s' % p.date.strftime("%Y%m%d"),
-			   0.5-(yearsold/10.0))
