745ed897ecad101249b5dda10d6af397e098ba1f
Magnus Hagander <magnus@hagander.net>
2012-03-02 12:01:08 +0100

    Show previous 10 posts by an organisation when moderating news
    This makes it a lot easier to see if they violate a policy or not...

--- pgweb/news/admin.py
+++ pgweb/news/admin.py
@@ -7,5 +7,13 @@ class NewsArticleAdmin(MarkdownPreviewAdmin):
 	list_display = ('title', 'org', 'date', 'approved', )
 	list_filter = ('approved', )
 	search_fields = ('content', 'title', )
+	change_form_template = 'admin/news/newsarticle/change_form.html'
+
+	def change_view(self, request, object_id, extra_context=None):
+		newsarticle = NewsArticle.objects.get(pk=object_id)
+		my_context = {
+			'latest': NewsArticle.objects.filter(org=newsarticle.org)[:10]
+		}
+		return super(NewsArticleAdmin, self).change_view(request, object_id, extra_context=my_context)
 
 admin.site.register(NewsArticle, NewsArticleAdmin)
