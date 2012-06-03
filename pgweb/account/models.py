f1ee1f4d72d29725049c964a17d68fe8a3fc1cd9
Magnus Hagander <magnus@hagander.net>
2012-06-03 12:26:33 +0200

    Add a help text to communtity auth title
    I keep forgetting that it's end-user-visible myself, so I'm sure others
    using it in the future could be equally confused :-)

--- pgweb/account/models.py
+++ pgweb/account/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 class CommunityAuthSite(models.Model):
-	name = models.CharField(max_length=100, null=False, blank=False)
+	name = models.CharField(max_length=100, null=False, blank=False,
+							help_text="Note that the value in this field is shown on the login page, so make sure it's user-friendly!")
 	redirecturl = models.URLField(max_length=200, null=False, blank=False)
 	cryptkey = models.CharField(max_length=100, null=False, blank=False,
 								help_text="Use tools/communityauth/generate_cryptkey.py to create a key")
