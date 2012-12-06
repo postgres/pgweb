24f6300928e7338a10e7f9e57e34879c87190d68
Magnus Hagander <magnus@hagander.net>
2012-12-06 19:40:47 +0100

    Add ability to approve events by batch

--- pgweb/events/admin.py
+++ pgweb/events/admin.py
@@ -3,9 +3,18 @@ from django.contrib import admin
 from util.admin import PgwebAdmin
 from models import *
 
+def approve_event(modeladmin, request, queryset):
+	# We need to do this in a loop even though it's less efficient,
+	# since using queryset.update() will not send the moderation messages.
+	for e in queryset:
+		e.approved = True
+		e.save()
+approve_event.short_description = 'Approve event'
+
 class EventAdmin(PgwebAdmin):
 	list_display = ('title', 'org', 'startdate', 'training', 'approved',)
 	list_filter = ('approved','training',)
 	search_fields = ('summary', 'details', 'title', )
+	actions = [approve_event, ]
 
 admin.site.register(Event, EventAdmin)
