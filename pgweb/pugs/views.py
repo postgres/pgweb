b52ed662b2187b1367ae71b18775b168878d1772
Jonathan S. Katz <jonathan.katz@excoventures.com>
2013-05-08 16:37:06 -0400

    Proper appending of pug list

--- pgweb/pugs/views.py
+++ pgweb/pugs/views.py
@@ -12,11 +12,11 @@ def index(request):
 	pug_list = []
 	for pug in PUG.objects.filter(approved=True).order_by('country__name', 'title').all():
 		if pug_list and pug_list[-1].get('country') == pug.country.name:
-			pug_list['pugs'].append(pug)
+			pug_list[-1]['pugs'].append(pug)
 		else:
 			pug_list.append({
-			'country': pug.country.name,
-			'pugs': [pug]
+				'country': pug.country.name,
+				'pugs': [pug]
 			})
 	return render_to_response('pugs/index.html', {
 		'pug_list': pug_list,
