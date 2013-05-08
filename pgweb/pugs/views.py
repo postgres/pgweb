87877bd536fa2e9ff220ef25f94d28590ebcbd9c
Jonathan S. Katz <jonathan.katz@excoventures.com>
2013-05-08 16:25:24 -0400

    Fixed indentation on pugs/views

--- pgweb/pugs/views.py
+++ pgweb/pugs/views.py
@@ -6,18 +6,18 @@ from pgweb.util.contexts import NavContext
 from models import PUG
 
 def index(request):
-    """
-    contains list of PUGs, in country/locale alphabetical order
-    """
-    pug_list = []
-    for pug in PUG.objects.filter(approved=True).order_by('country__name', 'title').all():
-        if pug_list and pug_list[-1].get('country') == pug.country.name:
-            pug_list['pugs'].append(pug)
-        else:
-            pug_list.append({
-                'country': pug.country.name,
-                'pugs': [pug]
-            })
-    return render_to_response('pugs/index.html', {
-        'pug_list': pug_list,
-    }, NavContext(request, 'community'))
+	"""
+	contains list of PUGs, in country/locale alphabetical order
+	"""
+	pug_list = []
+	for pug in PUG.objects.filter(approved=True).order_by('country__name', 'title').all():
+		if pug_list and pug_list[-1].get('country') == pug.country.name:
+			pug_list['pugs'].append(pug)
+		else:
+			pug_list.append({
+			'country': pug.country.name,
+			'pugs': [pug]
+			})
+	return render_to_response('pugs/index.html', {
+		'pug_list': pug_list,
+	}, NavContext(request, 'community'))
