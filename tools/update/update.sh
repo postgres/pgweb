c4a72a33174566feaf43e193e95bb2632c9b5758
Magnus Hagander <magnus@hagander.net>
2013-08-18 19:40:04 +0200

    Update command should run the kill as sudo

--- tools/update/update.sh
+++ tools/update/update.sh
@@ -32,7 +32,7 @@ fi
 
 # Cause reload
 #echo Reloading website due to updates
-pkill -f pgweb/manage.py
+sudo pkill -f pgweb/manage.py
 
 # Update the file listing the latest update
 mv -f /tmp/pgweb.update lastupdate
