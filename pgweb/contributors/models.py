3966b4b20d26f99773ac4db6c194315f2a2e9c62
Magnus Hagander <magnus@hagander.net>
2013-12-29 14:11:37 +0100

    Add a link from contributors to their userid
    This will be used to allow users to edit their own contributor records.
    
    Requires SQL:
    ALTER TABLE contributors_contributor add column "user_id" integer REFERENCES "auth_user" ("id") DEFERRABLE INITIALLY DEFERRED;
    CREATE INDEX "contributors_contributor_user_id" ON "contributors_contributor" ("user_id");
    UPDATE contributors_contributor set user_id=(select id from auth_user where auth_user.email=contributors_contributor.email) where contributors_contributor.email is not null;

--- pgweb/contributors/models.py
+++ pgweb/contributors/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 from pgweb.util.bases import PgModel
 
@@ -25,6 +26,7 @@ class Contributor(PgModel, models.Model):
 	companyurl = models.URLField(max_length=100, null=True, blank=True)
 	location = models.CharField(max_length=100, null=True, blank=True)
 	contribution = models.TextField(null=True, blank=True)
+	user = models.ForeignKey(User, null=True, blank=True)
 
 	purge_urls = ('/community/contributors/', )
 
