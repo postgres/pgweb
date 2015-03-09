1fd8343095d8a6f82eee1e7ae0bbab32d4120b01
Jonathan S. Katz <jonathan.katz@excoventures.com>
2015-03-09 13:38:44 -0400

    Ordered the PUG listings by Country, Locale as opposed to Country, Name

--- pgweb/pugs/views.py
+++ pgweb/pugs/views.py
@@ -9,7 +9,7 @@ def index(request):
 	contains list of PUGs, in country/locale alphabetical order
 	"""
 	pug_list = []
-	for pug in PUG.objects.filter(approved=True).order_by('country__name', 'title').all():
+	for pug in PUG.objects.filter(approved=True).order_by('country__name', 'locale').all():
 		if pug_list and pug_list[-1].get('country') == pug.country.name:
 			pug_list[-1]['pugs'].append(pug)
 		else:
