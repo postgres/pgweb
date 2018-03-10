10b8ee7de4a311f2916bd14764593ec001bcbe89
Magnus Hagander <magnus@hagander.net>
2018-03-10 10:57:55 -0500

    Fix context processor class name for newer django
    Missed this in 09f904f787616c19080032e1dfe0ef08404007c7

--- pgweb/settings.py
+++ pgweb/settings.py
@@ -71,7 +71,7 @@ TEMPLATES = [{
 		'context_processors': [
 			'django.contrib.auth.context_processors.auth',
 			'django.contrib.messages.context_processors.messages',
-			'django.core.context_processors.media',
+			'django.template.context_processors.media',
 			'pgweb.util.contexts.PGWebContextProcessor',
 		],
 	},
