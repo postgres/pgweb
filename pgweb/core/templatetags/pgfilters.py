f76b20304dfcee9073b8b678a4c7b6110d198774
Jonathan S. Katz <jonathan.katz@excoventures.com>
2018-04-15 13:42:49 -0400

    Alphabetize functions in pgfilters for easier mainetnance.

--- pgweb/core/templatetags/pgfilters.py
+++ pgweb/core/templatetags/pgfilters.py
@@ -2,11 +2,6 @@ from django.template.defaultfilters import stringfilter
 from django import template
 register = template.Library()
 
-@register.filter(name='hidemail')
-@stringfilter
-def hidemail(value):
-	return value.replace('@', ' at ')
-
 @register.filter(name='class_name')
 def class_name(ob):
 	return ob.__class__.__name__
@@ -14,6 +9,11 @@ def class_name(ob):
 @register.filter(is_safe=True)
 def field_class(value, arg):
 	return value.as_widget(attrs={"class": arg})
+
+@register.filter(name='hidemail')
+@stringfilter
+def hidemail(value):
+	return value.replace('@', ' at ')
 
 @register.filter(is_safe=True)
 def ischeckbox(obj):
