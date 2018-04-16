75feea184347179949f3aa5fc23f48736f88b0bf
Jonathan S. Katz <jonathan.katz@excoventures.com>
2018-04-15 23:44:49 -0400

    Update Documentation and Bug Report pages to use modern design.

--- pgweb/misc/views.py
+++ pgweb/misc/views.py
@@ -51,11 +51,12 @@ def submitbug(request):
 	return render_pgweb(request, 'support', 'base/form.html', {
 		'form': form,
 		'formitemtype': 'bug report',
+		'formtitle': 'Submit Bug Report <i class="fas fa-bug"></i>',
 		'operation': 'Submit',
 		'form_intro': template_to_string('misc/bug_header.html', {
 			'supportedversions': versions,
 		}),
-		'savebutton': 'Create report and send email',
+		'savebutton': 'Submit Report and Send Email',
 	})
 
 
