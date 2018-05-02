efcbcb18a57ca97c87c2cfb3aef3906129656295
Jonathan S. Katz <jonathan.katz@excoventures.com>
2018-05-02 12:05:49 -0400

    Fix generation of static templates URLs for sitemap.xml
    Cause of issue is speculated to be due to different relative path
    handling since upgrade of Django versions.  For the static pages,
    the path is handled relative to the file that is generating the
    URLs for the sitemap.

--- pgweb/core/struct.py
+++ pgweb/core/struct.py
@@ -6,8 +6,10 @@ def get_struct():
 	yield ('support/versioning', None)
 
 	# Enumerate all the templates that will generate pages
-	for root, dirs, files in os.walk('../templates/pages'):
-		r=root[19:] # Cut out ../templates/pages
+	pages_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../templates/pages/'))
+	for root, dirs, files in os.walk(pages_dir):
+		# Cut out the reference to the absolute root path
+		r = '' if root == pages_dir else os.path.relpath(root, pages_dir)
 		for f in files:
 			if f.endswith('.html'):
 				yield (os.path.join(r, f)[:-5] + "/",
