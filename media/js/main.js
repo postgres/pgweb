a33bc4630ba3b686ec1b9171caf541d52e1e2d74
Magnus Hagander <magnus@hagander.net>
2018-12-20 11:21:21 +0100

    Move debian/ubuntu downloads javascript into main.js

--- media/js/main.js
+++ media/js/main.js
@@ -3,3 +3,12 @@ $(document).ready(function() {
     $(".navbar").toggleClass("compressed", $(window).scrollTop() >= 20);
   });
 });
+
+
+/*
+ * Debian/Ubuntu download dropdowns
+ */
+function updateDebianSeries(select) {
+    var deb = document.getElementById('series-deb');
+    deb.innerHTML = select.value;
+}
