c1c40208ca518e974fa147d31d7e0a700d87d064
Magnus Hagander <magnus@hagander.net>
2018-12-22 16:24:10 +0100

    Another try at fixing google analytics vs CSP

--- pgweb/util/middleware.py
+++ pgweb/util/middleware.py
@@ -42,7 +42,7 @@ class PgMiddleware(object):
 		sources = OrderedDict([
 			('default', ["'self'", ]),
 			('img', ['*', ]),
-			('script', ["'self'", "google-analytics.com"]),
+			('script', ["'self'", "www.google-analytics.com", "ssl.google-analytics.com"]),
 			('media', ["'self'", ]),
 			('style', ["'self'", "fonts.googleapis.com"]),
 			('font', ["'self'", "fonts.gstatic.com"]),
