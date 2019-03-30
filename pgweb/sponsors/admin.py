e373e5f883a735debe2927c185193db66a2729e4
Jonathan S. Katz <jonathan.katz@excoventures.com>
2019-03-30 11:51:14 -0400

    Add more administrative features for managing sponsors.
    Specifically, being able to search by sponsor, filter by sponsor type, and
    make it easier to see some key information.

--- pgweb/sponsors/admin.py
+++ pgweb/sponsors/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Sponsor, SponsorType, Server
 
+
+class SponsorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'sponsortype', 'country']
+    list_filter = ['sponsortype']
+    search_fields = ['name']
+
+
 admin.site.register(SponsorType)
-admin.site.register(Sponsor)
+admin.site.register(Sponsor, SponsorAdmin)
 admin.site.register(Server)
