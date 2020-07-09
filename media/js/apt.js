6b61af92f727f6d13c22a5552a711a14bca1b2f5
Dave Page <dpage@pgadmin.org>
2020-07-09 12:03:28 +0100

    Move all the non-Yum event handler setup into one shared script, per discussion.

--- media/js/apt.js
+++
@@ -1,8 +0,0 @@
-/* Event handlers */
-function setupHandlers() {
-    document.getElementById('copy-btn').addEventListener('click', function () {
-        copyScript(this, 'script-box');
-    });
-}
-
-document.addEventListener("DOMContentLoaded", setupHandlers);
--- media/js/download.js
+++ media/js/download.js
@@ -1,14 +1,27 @@
 /* Event handlers */
 function setupHandlers() {
-    document.getElementById('btn-download-bsd').addEventListener('click', function (event) {
-        showDistros(this, 'download-subnav-bsd');
-        event.preventDefault();
-    });
+    /* BSD button on /download */
+    if (document.getElementById("btn-download-bsd")) {
+        document.getElementById('btn-download-bsd').addEventListener('click', function (event) {
+            showDistros(this, 'download-subnav-bsd');
+            event.preventDefault();
+        });
+    }
 
-    document.getElementById('btn-download-linux').addEventListener('click', function (event) {
-        showDistros(this, 'download-subnav-linux');
-        event.preventDefault();
-    });
+    /* Linux button on /download */
+    if (document.getElementById("btn-download-linux")) {
+        document.getElementById('btn-download-linux').addEventListener('click', function (event) {
+            showDistros(this, 'download-subnav-linux');
+            event.preventDefault();
+        });
+    }
+
+    /* Copy Script button on /download/linux/debian and /download/linux/ubuntu */
+    if (document.getElementById("copy-btn") && document.getElementById("script-box")) {
+        document.getElementById('copy-btn').addEventListener('click', function () {
+            copyScript(this, 'script-box');
+        });
+    }
 }
 
 document.addEventListener("DOMContentLoaded", setupHandlers);
