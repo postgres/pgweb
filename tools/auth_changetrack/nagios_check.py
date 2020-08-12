1ffc1f3d6d4a3346813bd786b96f8eab4a34a1b4
Magnus Hagander <magnus@hagander.net>
2020-08-12 17:14:19 +0200

    Update nagios monitor to check for duplicated email addresses
    This should not be possible if things are done through the appropriate
    interfaces, and those interfaces are bug-free (right..). But really bad
    things can probably happen if they don't, so put a monitor in place to
    check for it.
    
    This also adds a view in the db that shows all registered email
    addresses and their accounts, regardless of if it's primary or secondary
    address. This is used by the nagios check but can of course be useful to
    manual checks as well.

--- tools/auth_changetrack/nagios_check.py
+++ tools/auth_changetrack/nagios_check.py
@@ -9,6 +9,37 @@ WARNING_THRESHOLD = timedelta(minutes=5)
 # More than 15 minutes something is definitely wrong
 CRITICAL_THRESHOLD = timedelta(minutes=15)
 
+
+def check_queue(curs):
+    # Get the oldest entry that has not been completed, if any
+    curs.execute("SELECT COALESCE(now()-changedat) FROM account_communityauthchangelog")
+    rows = curs.fetchall()
+
+    if len(rows) == 0:
+        return "queue is empty"
+        sys.exit(0)
+
+    age = rows[0][0]
+
+    if age < WARNING_THRESHOLD:
+        return "queue age is %s" % age
+    elif age < CRITICAL_THRESHOLD:
+        print("WARNING, queue age is %s" % age)
+        sys.exit(1)
+    else:
+        print("CRITICAL, queue age is %s" % age)
+        sys.exit(2)
+
+
+def check_mail(curs):
+    curs.execute("SELECT count(*) FROM (SELECT 1 FROM all_user_email_addresses GROUP BY email HAVING count(*) > 1) x")
+    num, = curs.fetchone()
+    if num > 0:
+        print("CRITICAL, {} email addresses have duplicate entries!".format(num))
+        sys.exit(2)
+    return ""
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nagios_check.py <dsn>")
@@ -17,23 +48,8 @@ if __name__ == "__main__":
     conn = psycopg2.connect(sys.argv[1])
     curs = conn.cursor()
 
-    # Get the oldest entry that has not been completed, if any
-    curs.execute("SELECT COALESCE(max(now()-changedat), '0') FROM account_communityauthchangelog")
-    rows = curs.fetchall()
-    conn.close()
+    status = []
+    status.append(check_queue(curs))
+    status.append(check_mail(curs))
 
-    if len(rows) == 0:
-        print("OK, queue is empty")
-        sys.exit(0)
-
-    age = rows[0][0]
-
-    if age < WARNING_THRESHOLD:
-        print("OK, queue age is %s" % age)
-        sys.exit(0)
-    elif age < CRITICAL_THRESHOLD:
-        print("WARNING, queue age is %s" % age)
-        sys.exit(1)
-    else:
-        print("CRITICAL, queue age is %s" % age)
-        sys.exit(2)
+    print("OK: {}".format('; '.join([s for s in status if s])))
