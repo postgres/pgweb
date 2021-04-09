368550f2ee06db9e40099da36e133b2e291cf6f8
Magnus Hagander <magnus@hagander.net>
2021-04-09 15:20:56 +0200

    Exclude security notices with no CVE from list of CVEs in sitemap
    Otherwise we're instructing google and others to index pages that don't
    exist.

--- pgweb/security/struct.py
+++ pgweb/security/struct.py
@@ -5,5 +5,5 @@ from .models import SecurityPatch
 def get_struct():
     """create sitemap entries for each CVE entry and the top level CVE URL"""
     yield ('support/security/', None)
-    for s in SecurityPatch.objects.filter(public=True).order_by('-cvenumber'):
+    for s in SecurityPatch.objects.filter(public=True).exclude(cve='').order_by('-cvenumber'):
         yield ('support/security/CVE-{}'.format(s.cve), None)
