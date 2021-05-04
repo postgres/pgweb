f7fdc2c594c6c230d57c9c80c71c37988c74502a
Magnus Hagander <magnus@hagander.net>
2021-05-04 11:09:18 +0200

    Refresh contents of data fixtures
    This still needs to be done manually, but at least now the commands to
    do it are listed in the the loading scripts.

--- pgweb/load_initial_data.sh
+++ pgweb/load_initial_data.sh
@@ -3,6 +3,11 @@
 # We keep this in a separate script because using initial_data.xxx in django will overwrite
 # critical data in the database when running a 'syncdb'. We'd like to keep the ability to
 # run syncdb on updates...
+#
+# To create the dump files, use:
+# ./python manage.py dumpdata --indent 1 --format json core.version core.importedrssfeed core.country > pgweb/core/fixtures/data.json
+# ./python manage.py dumpdata --indent 1 --format json contributors.contributortype > pgweb/contributors/fixtures/data.json
+
 
 echo WARNING: this may overwrite some data in the database with an initial set of data.
 echo 'Are you sure you want this (answer "yes" to overwrite)'
