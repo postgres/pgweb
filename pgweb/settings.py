eaf08354dd2d1999dc99cd07d4fa22fc329ed9d1
Magnus Hagander <magnus@hagander.net>
2021-11-07 16:11:41 +0100

    Add missing context processor
    Needed for some admin functionality, shows up as a warning in 3.2.

--- pgweb/settings.py
+++ pgweb/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [{
     'DIRS': ['templates', ],
     'OPTIONS': {
         'context_processors': [
+            'django.template.context_processors.request',
             'django.contrib.auth.context_processors.auth',
             'django.contrib.messages.context_processors.messages',
             'django.template.context_processors.media',
