8169f72d7be706fce31630eafa3800ec647feda4
Magnus Hagander <magnus@hagander.net>
2024-04-09 15:30:39 +0200

    Update force_text -> force_str for Django 4.2

--- pgweb/core/templatetags/pgmarkdown.py
+++ pgweb/core/templatetags/pgmarkdown.py
@@ -1,6 +1,6 @@
 # Filter wrapping the python markdown library into a django template filter
 from django import template
-from django.utils.encoding import force_text
+from django.utils.encoding import force_str
 from django.utils.safestring import mark_safe
 
 from pgweb.util.markup import pgmarkdown
@@ -23,7 +23,7 @@ def markdown(value, args=''):
                 raise ValueError("Invalid argument to markdown: {}".format(a))
 
     return mark_safe(pgmarkdown(
-        force_text(value),
+        force_str(value),
         allow_images=allow_images,
         allow_relative_links=allow_relative_links,
     ))
