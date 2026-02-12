postgres/pgweb

commit fc120c1644dd623ff7509d39d82845e2b33233c4

Add @img suffix to cid values
Turns out this is required to be spec-compliant. I don't think this was
why things broke in my tests, but it is good to be comlpiant.

pgweb/news/util.py
@@ -41,7 +41,7 @@ def render_news_template(news):
                 'contenttype': '{}; name={}'.format(_get_contenttype_from_extension(f), f),
                 'filename': f,
                 'disposition': 'inline; filename="{}"'.format(f),
-                'id': '<{}>'.format(f),
+                'id': '<{}@img>'.format(f),
             }
             with open(os.path.join(basedir, f), "rb") as f:
                 a['content'] = f.read()

commit 727655b5c01025fcf87366f342b27a75df22ff00

Fix for email rendering preview on pgweb
fc120c16 introduced some "spec compliant" code for rendering image,
but it broke our lookup for the image in the repo. This fixes it.

pgweb/news/util.py
@@ -57,7 +57,7 @@ def embed_images_in_html(html, attachments):
     amap = {a['filename']: a for a in attachments}
 
     def _replace_cid_reference(t):
-        a = amap[t.group(2).replace('cid:', '')]
+        a = amap[t.group(2).replace('cid:', '').replace('@img', '')]
         datasrc = 'data:{};base64,{}'.format(a['mimetype'], base64.b64encode(a['content']).decode('ascii'))
         return t.group(1) + datasrc + t.group(3)
 