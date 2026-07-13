8ee6727632288fb1752c5de92bfcda0a7bbf9b2a
Magnus Hagander <magnus@hagander.net>
2026-07-13 17:03:45 +0200

    Add missing support for sk-ssh-ed25519@openssh.com
    Allow community logins to include sk-ssh-ed25519@openssh.com as an ssh
    key, which is what is used when a key is on a secure external device.
    
    Noted by Melanie Plageman

--- pgweb/core/models.py
+++ pgweb/core/models.py
@@ -254,7 +254,7 @@ class ImportedRSSItem(models.Model):
 
 
 # From man sshd, except for ssh-dss
-_valid_keytypes = ['ecdsa-sha2-nistp256', 'ecdsa-sha2-nistp384', 'ecdsa-sha2-nistp521', 'ssh-ed25519', 'ssh-rsa']
+_valid_keytypes = ['ecdsa-sha2-nistp256', 'ecdsa-sha2-nistp384', 'ecdsa-sha2-nistp521', 'ssh-ed25519', 'ssh-rsa', 'sk-ssh-ed25519@openssh.com']
 
 
 # Options, keytype, key, comment. But we don't support options.
